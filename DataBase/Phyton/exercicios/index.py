e2c62afc7577b9ab9193a70e17a45f23657f1506
adao45 <167785296+adao45@users.noreply.github.com>
2024-06-14 11:30:18 -0300

    14061

--- DataBase/Phyton/exercicios/index.py
+++ DataBase/Phyton/exercicios/index.py
@@ -1,78 +1,155 @@
-#Importação das bibliotecas
-import os
+import os;
 import produtos;
 
-#Declaração das variáveis
-inputMenu= int(-1)
+inputMenu = int(0);
 
-
-#Repetição para criação do menu
-while(inputMenu!=0):
-   
-    print('Digite 1 para a inserção de um produto');
+while( True ):
+    os.system('cls');
+    print('Menu de Seleção.');
+    print('Digite 1 para criar um produto');
     print('Digite 2 para visualizar todos os produtos');
-    print('Digite 3 para procurar um produto');
-    print('digite 4 para atualizar um produto');
-    print('Digite 5 para excluir um produto');
-    print('Digite 0 para encerrar')
+    print('Digite 3 para procurar');
+    print('Digite 4 para atualizar');
+    print('Digite 5 para excluir');
+    print('Digite 0 para encerrar');
 
-    inputMenu=int(input("\nDigite um valor do menu "))
+    inputMenu = int(input('Digite: '));
+
+    if ( inputMenu == 0 ):
+        break;
     
-    if (inputMenu ==1 ):
-       try:
-           name=str('');
-           price=int(0);
-           quantity=int(0);
-           print(f'\n Digite os dados solicitados para criação do produto.');
-           name=str(input('Nome: '));
-           price=float(input('Preço: '));
-           quantity=int(input('Quantidade: ')); 
-            
-           produtos.create(name,price,quantity);
-       except:
-           print('Something went wrong')
+    if ( inputMenu == 1 ):
+        try:
+            name = str('');
+            price = float(0);
+            quantity = int(0);
         
+            print(f'\nDigite os dados solicitados para a criação do produto.');
+            name = str(input('Nome: '));
+            price = float(input('Preço: '));
+            quantity = int(input('Quantidade: '));
+        
+            response = produtos.create(name,price,quantity);
+        
+            print(f'{response['message']}');
+    
+        except:
+            print('Something went wrong');
 
-    if(inputMenu == 2):
-        data=[];
-        data=produtos.select();
+    # SELECT
+    if ( inputMenu == 2 ):
+        data = [];
+        data = produtos.select();
 
-        #Exibir dados corretamente
-        if(data['code']==200):
-
+        # Exibir dados corretamente
+        if ( data['code'] == 200 ):
+            
             for product in data['data']:
-                print(f'\nNome: {product[0]}, Preço: {product[1]}, Quantidade:  {product[2]} un')
+                print(f'\nNome: {product[0]} , Preço: {product[1]} R$ , ' + 
+                      f'Quantidade: {product[2]} un')
 
-        else: 
-        #Erro de banco de dados
-            if(data['code']==500):
-                print(f'\n{data['data']}')
+        else:
+            # Erro de banco de dados
+            if ( data['code'] == 500 ):
+                print(f'\n{data['message']}');
             else:
+                # Erro não previsto
                 print(f'\nUnknown Error');
     
-    #Find
-    if (inputMenu == 3):
-        data=[];
-        name=str('');
+    # FIND
+    if ( inputMenu == 3 ):
+        data = [];
+        name = str('');
         try:
             print('\nProcurar produto específico.');
-            name=input('Nome do Produto: ');
-            
-            data=produtos.findByName(name);
+            name = input('Nome do produto: ');
+        
+            data = produtos.findByName(name);
 
-            #Exibir dados corretamente
-            if(data['code']==200):
-                print(f'\nNome: {data['data'][0]}, Preço: {data['data'][1]}, Quantidade: {data['data'][2]} un')          
-                
-            else:    
-                #Produto não encontrado ou erro de banco de dados
-                if (data['code']==404 or data['code']==500):
+            # Exibir dados corretamente
+            if ( data['code'] == 200 ):
+                print(f'\nNome: {data['data'][0]} , Preço: {data['data'][1]} R$ , ' + 
+                      f'Quantidade: {data['data'][2]} un')
+            else:
+                # Produto não encontrado ou erro de banco de dados
+                if ( data['code'] == 404 or data['code'] == 500):
                     print(f'\n{data['message']}');
                 else:
-                    #Erro não previsto    
+                    # Erro não previsto
                     print(f'\nUnknown Error');
-        except:
-            print('Somenthing went wrong')     
 
-    input ('\nDigite enter para continuar..');
-    os.system('cls')
+        except:
+            print('Something went wrong');
+    
+    if ( inputMenu == 4 ):
+        inputSubMenu = int(0);
+
+        print('\nMenu de atualização.');
+        print('Digite 1 para atualizar o nome.');
+        print('Digite 2 para atualizar o preço.');
+        print('Digite 3 para atualizar a quantidade.');
+
+        try:
+            inputSubMenu = int(input('Digite: '));
+
+            if (inputSubMenu == 1):
+                name = str('');
+                print(f'\nAtualizar o Nome de um produto.');
+                name= input('Nome do produto: ');
+                product = produtos.findByName(name)
+                if(product['code'] == 404):
+                   print(f'\n{product['message']}') 
+                else:                                 
+                    newName=input('Digite o nome Nome: ');           
+                    data = produtos.updateName(name, newName);
+                    print(f'\n{data['message']}')
+        except:
+            print('Somenthing went wrong');
+
+        try:
+            if (inputSubMenu == 2):
+                name = str('');
+                price = float(0);
+                print(f'\nAtualizar o preço de um produto.');
+                name= input('Nome do produto: ');
+                price = float(input('Novo preço: '));
+        
+                data = produtos.updatePrice(name, price);
+                print(f'\n{data['message']}')
+        except:
+            print('Somenthing went wrong');
+
+        try:
+            if (inputSubMenu == 3):
+                name = str('');
+                quantity = float(0);
+                print(f'\nAtualizar a quantidade de um produto.');
+                name= input('Nome do produto: ');
+                product = produtos.findByName(name)
+                if(product['code'] == 404):
+                    print(f'\n{product['message']}') 
+                else:
+                    quantity= float(input('Nova quantidade: '));
+                    data = produtos.updatequantity(name, quantity);
+                    print(f'\n{data['message']}')
+        except:
+            print('Somenthing went wrong');
+
+
+    # DELETE
+    if ( inputMenu == 5 ):
+        data = [];
+        name = str('');
+
+        try:
+            print('\nDeletar um produto.');
+            name = input('Nome do produto: ');
+        
+            data = produtos.delete(name);
+
+            print( f'\n{data['message']}' );
+
+        except:
+            print('Something went wrong');
+
+    input('\nAperte ENTER para continuar.');
--- DataBase/Phyton/exercicios/produtos.py
+++ DataBase/Phyton/exercicios/produtos.py
@@ -1,60 +1,145 @@
-import sqlite3
+import sqlite3;
 
-#Conecta com banco de dados
 connection = sqlite3.connect('Store.db');
-cursor = connection.cursor() #possibilita que o comando seja executado
+cursor = connection.cursor();
 
-#as aspas server para quebrar as linhas
+
 cursor.execute('''
-    CREATE TABLE IF NOT EXISTS products(
-               id INTEGER PRIMARY KEY,
-               name VARCHAR(32) UNIQUE NOT NULL,
-               price FLOAT NOT NULL,
-               quantity INTEGER NOT NULL);
-
-''')
+    CREATE TABLE IF NOT EXISTS Products (
+        id INTEGER PRIMARY KEY,
+        name VARCHAR(32) UNIQUE NOT NULL,
+        price FLOAT NOT NULL,
+        quantity INTEGER NOT NULL
+    );
+''');
 
 #connection.close();
 
-def create(name:str,price:int,quantity:int):
+def create(name:str , price:float , quantity:int):
     try:
+        productByName = findByName(name);
 
-        cursor.execute('INSERT INTO products (name,price,quantity) VALUES (?,?,?)', (name, price, quantity));
-        connection.commit();    
-        return{'code':201, 'message': 'product Created'};
+        if ( productByName['code'] == 200 ):
+            return {'code': 400, 'message': 'Product already exist '};
+
+        cursor.execute('INSERT INTO Products (name,price,quantity) VALUES (?,?,?)', (name , price , quantity));
+        connection.commit();
+    
+        return {'code' : 201 , 'message' : 'Product Created'};
     except:
-        return {'code':500, 'message': 'Internal Error'};
-                
+        return {'code': 500 , 'message' : 'Internal Error'};
 
-
-def finByName(name:str):
+def findByName(name:str):
     try:
-        cursor.execute('SELECT * FROM Products WHERE name =?', (name,));
+        cursor.execute('SELECT name,price,quantity FROM Products WHERE name = ?', (name,));
         product = cursor.fetchone();
+    
+        if ( product == None ):
+            return {'code': 404, 'message' : 'Product not found'};
+        
+        return {'code': 200, 'data': product};
 
-        if(product == None):
-            return {'code':404, 'message' : 'Product not found'};
-        return{'code':200, 'data': product};
     except:
-        return{'code':500, 'message' : 'Internal Error'};
-
+        return {'code': 500 , 'message' : 'Internal Error'};
 
 def select():
-
     try:
-        products=[];
-        cursor.execute('SELECT name,price,quantity FROM products')
-        products=cursor.fetchall();
- 
-        return {'code':200, 'data':products};
+        products = [];
+
+        cursor.execute('SELECT name,price,quantity FROM Products');
+        products = cursor.fetchall();
+        return { 'code': 200 , 'data': products};
+    
     except:
-        return {'code':500,'data': 'internal Error'}
+        return { 'code': 500 , 'message': 'Internal Error'};
+
+def updateName(name:str, newName=str):
+    try:
+        id = 0;
+        cursor.execute('SELECT id FROM Products WHERE name = ?', (name,));
+        product = cursor.fetchone();
+
+        if ( product == None ):
+            return {'code': 404, 'message': 'Product not found'};
+    
+        # A posição 0 de product é o ID
+        id = product[0];
+        
+        productByName = findByName(newName);
+        if(productByName['code']==200):
+            return {'code':400, 'message': 'Product name already exist'}
+            
+
+        cursor.execute('UPDATE Products SET name= ?  WHERE id = ?', (newName,id,));
+        connection.commit();
+
+        return {'code': 202, 'message': 'Product alterado successfully'}
+
+    except:
+        return { 'code': 500 , 'message': 'Internal Error'};
+
+
+def updatePrice(name:str, newPrice:float):
+    try:
+        id = 0;
+        cursor.execute('SELECT id FROM Products WHERE name = ?', (name,));
+        product = cursor.fetchone();
+        if ( product == None ):
+            return {'code': 404, 'message': 'Product not found'};
+    
+        # A posição 0 de product é o ID
+        id = product[0];
+        
+
+        cursor.execute('UPDATE Products SET Price= ?  WHERE id = ?', (newPrice,id,));
+        connection.commit();
+
+        return {'code': 202, 'message': 'Product alterado successfully'}
+
+    except:
+        return { 'code': 500 , 'message': 'Internal Error'};
+
+def updatequantity(name:str, newQuantity:float):
+    try:
+        id = 0;
+        cursor.execute('SELECT id FROM Products WHERE name = ?', (name,));
+        product = cursor.fetchone();
+        if ( product == None ):
+            return {'code': 404, 'message': 'Product not found'};
+    
+        # A posição 0 de product é o ID
+        id = product[0];
+        
+
+        cursor.execute('UPDATE Products SET quantity= ?  WHERE id = ?', (newQuantity,id,));
+        connection.commit();
+
+        return {'code': 202, 'message': 'Quantidade alterado successfully'}
+
+    except:
+        return { 'code': 500 , 'message': 'Internal Error'};
 
 
 
-def update():
-    pass;
 
-def delete():
-    pass;
+
+def delete(name:str):
+    try:
+        id = 0;
+        cursor.execute('SELECT id FROM Products WHERE name = ?', (name,));
+        product = cursor.fetchone();
+
+        if ( product == None ):
+            return {'code': 404, 'message': 'Product not found'};
+    
+        # A posição 0 de product é o ID
+        id = product[0];
+
+        cursor.execute('DELETE FROM Products WHERE id = ?', (id,));
+        connection.commit();
+
+        return {'code': 204, 'message': 'Product deleted successfully'}
+
+    except:
+        return { 'code': 500 , 'message': 'Internal Error'};
     
